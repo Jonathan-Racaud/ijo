7cc89c5b545b17d48acb15c36f9149a6f3fbaa71
Jonathan Racaud <jonathan.racaud@outlook.com>
2023-04-16 19:18:13 +0900

    Added skeleton of VM execution

--- include/ijoVM.h
+++ include/ijoVM.h
@@ -3,12 +3,27 @@
 
 #include "chunk.h"
 
+/// @brief The result of interpreting a Chunk of code.
+typedef enum {
+    /// @brief Code interpretation was successful.
+    INTERPRET_OK,
+
+    /// @brief An error occured while compiling code.
+    INTERPRET_COMPILE_ERROR,
+
+    /// @brief An error occured while running code.
+    INTERPRET_RUNTIME_ERROR
+} InterpretResult;
+
 /**
  * @brief The ijo Virtual Machine.
  */
 typedef struct {
     /// @brief The chunk of code to execute.
     Chunk *chunk;
+
+    /// @brief Current instruction pointer.
+    uint32_t *ip;
 } ijoVM;
 
 /**
@@ -21,5 +36,20 @@ ijoVM *ijoVMNew();
  * @brief Deletes an @p ijoVM.
  */
 void ijoVMDelete(ijoVM *vm);
+
+/**
+ * @brief Interprets a chunk of code.
+ * @param vm The ijoVM that should interpret the code.
+ * @param chunk The chunk of code to be interpreted.
+ * @return The result of the code interpretation.
+ */
+InterpretResult ijoVMInterpret(ijoVM *vm, Chunk *chunk);
+
+/**
+ * @brief Runs the vm.
+ * @param vm The vm to run.
+ * @return The interpret result for this vm.
+ */
+InterpretResult ijoVMRun(ijoVM *vm);
 
 #endif // IJO_VM_H
--- src/ijoVM.c
+++ src/ijoVM.c
@@ -3,6 +3,10 @@
 #include "common.h"
 #include "log.h"
 
+#if DEBUG
+#include "debug.h"
+#endif
+
 ijoVM *ijoVMNew() {
     ijoVM* vm = (ijoVM*)malloc(sizeof(ijoVM));
 
@@ -13,4 +17,40 @@ ijoVM *ijoVMNew() {
 
 void ijoVMDelete(ijoVM *vm) {
     Delete(vm);
+}
+
+InterpretResult ijoVMInterpret(ijoVM *vm, Chunk *chunk) {
+    vm->chunk = chunk;
+    vm->ip = vm->chunk->code;
+
+    return ijoVMRun(vm);
+}
+
+InterpretResult ijoVMRun(ijoVM *vm) {
+#define READ_BYTE() (*vm->ip++)
+#define READ_CONST() (vm->chunk->constants.values[READ_BYTE()])
+
+    for(;;) {
+        #if DEBUG_TRACE_EXECUTION
+        DisassembleInstruction(vm->chunk, (uint32_t)(vm->ip - vm->chunk->code));
+        #endif
+
+        uint32_t instruction;
+
+        switch (instruction = READ_BYTE())
+        {
+        case OP_CONSTANT: {
+            Value constant = READ_CONST();
+        }
+        case OP_RETURN: {
+            return INTERPRET_OK;
+        }
+        
+        default:
+            return INTERPRET_RUNTIME_ERROR:
+        }
+    }
+
+#undef READ_CONST
+#undef READ_BYTE
 }
