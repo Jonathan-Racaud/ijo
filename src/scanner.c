cf38181c17a5f918e649cfe9a158a4f943b642b5
Jonathan Racaud <jonathan.racaud@outlook.com>
2023-04-17 19:05:49 +0900

    Added barebone of creating a token

--- src/scanner.c
+++ src/scanner.c
@@ -1,5 +1,15 @@
 #include "scanner.h"
 #include "ijoMemory.h"
+#include "log.h"
+
+// Private function forward declaration
+
+Token makeToken(Scanner *scanner, TokenType);
+Token errorToken(Scanner *scanner, const char *message);
+
+bool isAtEnd(Scanner *scanner);
+
+// Public functions implementations
 
 Scanner *ScannerNew() {
     Scanner *scanner = (Scanner*)malloc(sizeof(Scanner));
@@ -18,5 +28,37 @@ void ScannerInit(Scanner *scanner, const char *source) {
 }
 
 Token ScannerScan(Scanner *scanner) {
-    
+    scanner->start = scanner->current;
+
+    if (isAtEnd(scanner)) return makeToken(scanner, TOKEN_EOF);
+
+    return errorToken(scanner, "Unexpected character");
+}
+
+Token makeToken(Scanner *scanner, TokenType type) {
+    Token token = {
+        type,
+        scanner->start,
+        (int)(scanner->current - scanner->start),
+        scanner->line
+    };
+
+    return token;
+}
+
+Token errorToken(Scanner *scanner, const char *message) {
+    LogError("%s at line %s", message, scanner->line);
+
+    Token error = {
+        TOKEN_ERROR,
+        message,
+        (int)strlen(message),
+        scanner->line
+    };
+
+    return error;
+}
+
+bool isAtEnd(Scanner *scanner) {
+    return *scanner->current == '\0';
 }
